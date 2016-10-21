342425d2a33c958737d602101971de6cf6fa535c
timbarrass <tim.barrass@gmail.com>
2016-10-21 16:44:19 +0100

    Separated populate, and pack-and-draw operations

--- src/main/scala/client/jsClient.scala
+++ src/main/scala/client/jsClient.scala
@@ -14,6 +14,8 @@ import scala.util.Try
 @JSExport
 object jsClient {
 
+  var items = new mutable.MutableList[(String, Double)]
+
   @JSExport
   def main(target: org.scalajs.dom.raw.Element): Unit = {
 
@@ -30,16 +32,25 @@ object jsClient {
           <.div(
             ^.padding := 2,
             <.button(
-              "Repopulate and Pack",
-              ^.id := "repopulateAndPackButton",
-              ^.onClick --> addClickedMessage
+              "Repopulate",
+              ^.id := "repopulateButton",
+              ^.onClick --> randomPopulation
+            )
+          ),
+          <.div(
+            ^.padding := 2,
+            <.button(
+              "Repack",
+              ^.id := "repackButton",
+              ^.onClick --> repack
             )
           ),
           <.div(
             ^.padding := 2,
             <.input(
               ^.`type` := "file",
-              ^.onChange ==> onFileChosen
+              ^.onChange ==> onFileChosen,
+              ^.title := "Populate from file"
             )
           )
         )
@@ -51,48 +62,55 @@ object jsClient {
 
   def onFileChosen(e: ReactEventI): Callback = {
 
-    var items = new scala.collection.mutable.Queue[(String, Double)]()
+    var reader = new dom.FileReader()
 
-    var reader = new dom.FileReader();
-
-    reader.readAsText(e.currentTarget.files.item(0))
     reader.onload = (e: UIEvent) => {
       val lines = reader.result.asInstanceOf[String].split("\r\n")
 
-      for (line <- lines) {
-        println("line: " + line)
+      items = populationFromArray(lines)
 
-        val contents = line.split(",")
-
-        val value = parseDouble(contents(1))
-
-        println("data: " + contents(0) + "," + contents(1) + "," + value)
-
-        items.enqueue(new Tuple2[String, Double](contents(0), value match { case Some(value) => value case None => 0.0 }))
-      }
-
-      println("Parsed " + items.length + " items")
-
-      packAndDraw(items)
+      clearChart
     }
+
+    reader.readAsText(e.currentTarget.files.item(0))
 
     Callback.empty
   }
 
+  def populationFromArray(lines: Array[String]): mutable.MutableList[(String, Double)] = {
+    var items = new scala.collection.mutable.MutableList[(String, Double)]()
+
+    for (line <- lines) {
+      val contents = line.split(",")
+
+      val value = parseDouble(contents(1))
+
+      items += new Tuple2[String, Double](contents(0), value match { case Some(value) => value case None => 0.0 })
+    }
+
+    items
+  }
+
   def parseDouble(s: String): Option[Double] = Try { s.toDouble }.toOption
 
-  def addClickedMessage(): Callback = {
-    val items = packer.randomPopulation
-
+  def repack(): Callback = {
     packAndDraw(items)
 
     Callback.empty
   }
 
-  def packAndDraw(items: mutable.Queue[(String, Double)]): Unit = {
+  def randomPopulation(): Callback = {
+    items = packer.randomPopulation
+
+    clearChart
+
+    Callback.empty
+  }
+
+  def packAndDraw(items: mutable.MutableList[(String, Double)]): Unit = {
     val bins = new Bins(10)
 
-    while (items nonEmpty) packer.placeUsingAverageBestFit(bins, (items dequeue) _2)
+    for (item <- items) packer.placeUsingAverageBestFit(bins, item _2)
 
     clearChart
 
