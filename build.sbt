07492b3af1aa587f9b911f14ea649bfd0859b1c3
timbarrass <tim.barrass@gmail.com>
2016-10-21 16:16:06 +0100

    Added first cut of file load handling
    .. and also added pass of target element for react app from html to
    scala

--- build.sbt
+++ build.sbt
@@ -4,19 +4,13 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-offline := true
-
 enablePlugins(ScalaJSPlugin)
 
-libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.0"
+//libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.0"
 
-libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.9.0"
+//libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.9.0"
 
-libraryDependencies += "com.lihaoyi" %%% "scalatags" % "0.4.6"
-
-skip in packageJSDependencies := false
-jsDependencies +=
-  "org.webjars" % "jquery" % "2.1.4" / "2.1.4/jquery.js"
+//libraryDependencies += "com.lihaoyi" %%% "scalatags" % "0.4.6"
 
 // core = essentials only. No bells or whistles.
 libraryDependencies += "com.github.japgolly.scalajs-react" % "core_sjs0.6_2.11" % "0.11.2"
--- src/main/scala/client/jsClient.scala
+++ src/main/scala/client/jsClient.scala
@@ -5,52 +5,99 @@ import japgolly.scalajs.react.{ReactDOM, _}
 import japgolly.scalajs.react.vdom.prefix_<^._
 import org.scalajs.dom
 import org.scalajs.dom.html.Canvas
-import org.scalajs.dom.{CanvasRenderingContext2D, document}
+import org.scalajs.dom.{CanvasRenderingContext2D, UIEvent, document}
 
+import scala.collection.mutable
 import scala.scalajs.js.annotation.JSExport
+import scala.util.Try
 
 @JSExport
 object jsClient {
 
   @JSExport
-  def main: Unit = {
+  def main(target: org.scalajs.dom.raw.Element): Unit = {
 
     val theApp = ReactComponentB[Unit]("TheApp")
       .render($ => {
         <.div(
           <.div(
+            ^.padding := 2,
             <.canvas(
-              ^.id := "packedBinCanvas"
+              ^.id := "packedBinCanvas",
+              ^.height := 110
             )
           ),
           <.div(
+            ^.padding := 2,
             <.button(
               "Repopulate and Pack",
               ^.id := "repopulateAndPackButton",
               ^.onClick --> addClickedMessage
+            )
+          ),
+          <.div(
+            ^.padding := 2,
+            <.input(
+              ^.`type` := "file",
+              ^.onChange ==> onFileChosen
             )
           )
         )
       })
       .build
 
-    ReactDOM.render(theApp(), document.getElementById("main"))
+    ReactDOM.render(theApp(), target)
   }
 
-  def addClickedMessage(): Callback = {
-    val items = packer.randomPopulation
+  def onFileChosen(e: ReactEventI): Callback = {
 
-    val bins = new Bins(10)
+    var items = new scala.collection.mutable.Queue[(String, Double)]()
 
-    while(items nonEmpty) packer.placeUsingAverageBestFit(bins, (items dequeue) _2)
+    var reader = new dom.FileReader();
 
-    clearChart
+    reader.readAsText(e.currentTarget.files.item(0))
+    reader.onload = (e: UIEvent) => {
+      val lines = reader.result.asInstanceOf[String].split("\r\n")
 
-    drawChart(bins)
+      for (line <- lines) {
+        println("line: " + line)
+
+        val contents = line.split(",")
+
+        val value = parseDouble(contents(1))
+
+        println("data: " + contents(0) + "," + contents(1) + "," + value)
+
+        items.enqueue(new Tuple2[String, Double](contents(0), value match { case Some(value) => value case None => 0.0 }))
+      }
+
+      println("Parsed " + items.length + " items")
+
+      packAndDraw(items)
+    }
 
     Callback.empty
   }
 
+  def parseDouble(s: String): Option[Double] = Try { s.toDouble }.toOption
+
+  def addClickedMessage(): Callback = {
+    val items = packer.randomPopulation
+
+    packAndDraw(items)
+
+    Callback.empty
+  }
+
+  def packAndDraw(items: mutable.Queue[(String, Double)]): Unit = {
+    val bins = new Bins(10)
+
+    while (items nonEmpty) packer.placeUsingAverageBestFit(bins, (items dequeue) _2)
+
+    clearChart
+
+    drawChart(bins)
+  }
 
   def drawChart(bins: Bins): Unit = {
     // probably don't want to fetch this every time we draw anything ...
